577ce0998c35f50dfb0720b38e45c05b4eafdd14
Paolo Angelani <119342177+paoloangelani@users.noreply.github.com>
2023-05-31 18:15:33 +0200

    login page + submit page + folder backend

---
+++ src/Routes.js
@@ -0,0 +1,6 @@
+import LoginPage from '../src/pages/Login';
+import SignupPage from '../src/pages/SignupPage';
+export {
+    LoginPage,
+    SignupPage
+};
---
+++ src/pages/SignupPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Singup from '../components/Signup/Signup';
+
+
+const SignupPage = () => {
+  return (
+    <div>
+        <Singup/>
+    </div>
+  )
+}
+
+export default SignupPage
