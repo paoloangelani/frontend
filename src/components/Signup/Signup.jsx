d19b363aa7a968ecbe05241cf05cbb27ed75219e
Paolo Angelani <119342177+paoloangelani@users.noreply.github.com>
2023-06-01 12:29:38 +0200

    correct dependencis + error with axios

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -3,7 +3,8 @@ import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import styles from "../../styles/style";
 import { Link } from "react-router-dom";
 import { RxAvatar } from "react-icons/rx";
-
+import axios from "axios";
+import { server } from "../../server";
 
 const Signup = () => {
 
@@ -14,9 +15,7 @@ const Signup = () => {
     const [visible, setVisible] = useState("");
     const [avatar, setAvatar] = useState(null);
 
-const handleSubmit = () => {
-       console.log("kkddkdk");
-      }
+
 
 
 const handleFileInputChange = (e) => {
@@ -24,7 +23,29 @@ const handleFileInputChange = (e) => {
     setAvatar(file);
   };
 
-  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    
+    const config = { headers: { "Content-Type": "multipart/form-data" } };
+    const newForm = new FormData();
+
+    newForm.append("file", avatar);
+    newForm.append("name", name);
+    newForm.append("email", email);
+    newForm.append("password", password);
+
+
+    axios
+    .post(`${server}/user/create-user`, newForm, config)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+      
 
     return (
         <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
@@ -35,7 +56,7 @@ const handleFileInputChange = (e) => {
           </div>
           <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
             <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-              <form className="space-y-6">
+              <form className="space-y-6" onSubmit={handleSubmit}>
                 <div>
                   <label
                     htmlFor="email"
